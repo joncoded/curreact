67d6132812e257b39f5c85d075454eb2b61a676f
joncoded <joncoded@gmail.com>
2021-02-07 13:48:18 -0500

    adding sample sub-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import SampleComponent from './components/SampleComponent/SampleComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React!</h1>            
+      <h1>Welcome to React!</h1> 
+      <p>This piece is from a sample component: </p>           
+      <SampleComponent />
     </div>
   );
 }
---
+++ src/components/SampleComponent/SampleComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SampleComponent (props) {
+  return (
+    <h2>Sample Component</h2>
+  )
+}
+
+export default SampleComponent;
